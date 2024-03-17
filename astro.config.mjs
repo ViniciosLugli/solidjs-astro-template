c9f24ca33b0d140addc1491dbcb02a2b8dc999d1
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-17 14:40:14 -0300

    feat(astro): :recycle: Refactor layout code path and added compression plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from '@astrojs/vercel/serverless';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solid(), tailwind()],
+	integrations: [solid(), tailwind(), compress()],
 	output: 'server',
 	adapter: vercel(),
 });
