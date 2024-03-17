858e0fdc221883eed3f0afa4252816bdbb937e28
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-17 15:51:00 -0300

    style(format): :art: Format the astro configcode

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,12 @@ import compress from 'astro-compress';
 import partytown from '@astrojs/partytown';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
-
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://solidjs-astro-template.vercel.app',
-  integrations: [solid(), tailwind(), compress(), partytown(), sitemap(), robotsTxt(), icon()],
-  output: 'server',
-  adapter: vercel()
+	site: 'https://solidjs-astro-template.vercel.app',
+	integrations: [solid(), tailwind(), compress(), partytown(), sitemap(), robotsTxt(), icon()],
+	output: 'server',
+	adapter: vercel(),
 });
