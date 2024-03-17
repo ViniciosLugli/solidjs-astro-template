f789382165f5f28638f0751a92bcfb759fafbef3
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-17 15:35:10 -0300

    feat(vercel): :rocket: Update SEO package site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://solidjs-astro-template.vercel.app',
 	integrations: [solid(), tailwind(), compress(), partytown(), sitemap(), robotsTxt()],
 	output: 'server',
 	adapter: vercel(),
