e530505322abd18b78cdabde40eb72b5abe6a4b6
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-17 15:26:13 -0300

    feat(packages): :package: Added SEO packages to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
+import partytown from '@astrojs/partytown';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solid(), tailwind(), compress()],
+	integrations: [solid(), tailwind(), compress(), partytown(), sitemap(), robotsTxt()],
 	output: 'server',
 	adapter: vercel(),
 });
